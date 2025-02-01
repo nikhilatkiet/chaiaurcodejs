d09cbd031a634f9af5b679afd3407a5f4623dd76
nikhilatkiet <nikhil.2428cs2126@kiet.edu>
2025-02-01 18:53:21 +0000

    more

---
+++ 01/02variables.js
@@ -0,0 +1,7 @@
+const AccountId=5656
+let accountEmail="homem@gmail.com"
+var accountPassword= "123" //var nhi use krte usme scope pe dikkat dega lacks block and functional scope
+accountCity= "Ayodhya" // can be done but dont use this
+let accountState
+
+console.table([AccountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01/03_datatypes.js
@@ -0,0 +1,13 @@
+"use strict";
+//it says that we are using the newer version of code. treat JS as newer version
+//alert(3+3) yha nhi kaam krega node me 
+let name="NIkhil"
+let age= 19
+let isloggedin= true
+// null is a standalone value assigned value is empty
+// undefined means we have not assigned value to it
+//symbol is used for uniqueness
+//objects
+console.log(typeof "Name")
+console.log(typeof null) // object is its type
+console.log(typeof undefined)
