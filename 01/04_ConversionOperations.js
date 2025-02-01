6cbc61acfee524eea1ced524a37515d8171b752b
nikhilatkiet <nikhil.2428cs2126@kiet.edu>
2025-02-01 19:06:30 +0000

    typecast

---
+++ 01/04_ConversionOperations.js
@@ -0,0 +1,14 @@
+let score= 22
+console.log(typeof score)
+console.log(typeof(score))  //output is number with small n
+let score1= "33"
+console.log(Number(score1)) // N is in capital
+//taking about numbers
+// "33" gives 33
+// true as 1
+// "33ab" gives nan meaning not a number but type is still number
+// null gives 0
+// also do Boolean 
+console.log(Boolean(score1))
+// "" gives false
+// "anyvalue" gives true
